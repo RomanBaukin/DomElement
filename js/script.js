12ca87c32b30eefdb7d7231bac36d96a02882dce
Roman Baukin <rbaukin1984@gmail.com>
2020-12-09 10:36:30 +0400

    Урок №14 усложненное задание

--- js/script.js
+++ js/script.js
@@ -9,16 +9,13 @@ const DomElement = function (selector, height, width, bg, fontSize) {
 };
 
 DomElement.prototype.createElement = function () {
-  let element = '',
-    newElement;
+  let newElement;
 
   if (this.selector[0] === '.') {
-    element = 'div';
-    newElement = document.createElement(element);
+    newElement = document.createElement('div');
     newElement.className = this.selector.substring(1);
   } else if (this.selector[0] === '#') {
-    element = 'p';
-    newElement = document.createElement(element);
+    newElement = document.createElement('p');
     newElement.id = this.selector.substring(1);
   }
   newElement.innerHTML = 'Классная штука - конструкторы!';
@@ -30,6 +27,33 @@ DomElement.prototype.createElement = function () {
   document.body.appendChild(newElement);
 };
 
-const firstElement = new DomElement('#first', '100px', '100px', 'red', '16px');
+const firstElement = new DomElement('.first', '100px', '100px', 'red', '16px');
 
-firstElement.createElement();
+document.addEventListener('DOMContentLoaded', function () {
+  firstElement.createElement();
+
+  let square = document.querySelector('.first');
+  square.style.position = 'absolute';
+
+  const moveSquare = function (e) {
+    let squareStyle = window.getComputedStyle(square);
+    let left = parseInt(squareStyle.left);
+    let top = parseInt(squareStyle.top);
+
+    switch (e.keyCode) {
+      case 37: // если нажата клавиша влево
+        square.style.left = left - 10 + 'px';
+        break;
+      case 38: // если нажата клавиша вверх
+        square.style.top = top - 10 + 'px';
+        break;
+      case 39: // если нажата клавиша вправо
+        square.style.left = left + 10 + 'px';
+        break;
+      case 40: // если нажата клавиша вниз
+        square.style.top = top + 10 + 'px';
+        break;
+    }
+  };
+  addEventListener('keydown', moveSquare);
+});
